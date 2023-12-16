4d8be6bec95990d25d0572778c3ecc93dcca6734
ndnghia2410 <91201271+ndnghia24@users.noreply.github.com>
2023-12-16 19:32:23 +0700

    add ViewModel for orders list

---
+++ lib/view_models/OrdersListViewModel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import '/models/DTO/OrderModel.dart';
+import '/data_sources/hardcode.dart' as data;
+
+class ListOrdersViewModel extends ChangeNotifier {
+  List<OrderModel> _orders = [];
+
+  List<OrderModel> get orders {
+    return this._orders;
+  }
+
+  Future<void> fetchOrders() async {
+    this._orders = data.orders.cast<OrderModel>();
+  }
+
+  List<OrderModel> getOrdersByStatus(String status) {
+    return this._orders.where((order) => order.status == status).toList();
+  }
+}
+
+class OrderViewModel extends ChangeNotifier {
+  final OrderModel _order;
+
+  OrderViewModel(this._order);
+}
