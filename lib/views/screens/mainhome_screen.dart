e9a560e8538a48764fc11f20a5f22704c51f61da
Hoang Do <hoang2003ngolike@gmail.com>
2023-12-17 06:11:31 +0700

    added basic slider for sales event

--- lib/views/screens/mainhome_screen.dart
+++ lib/views/screens/mainhome_screen.dart
@@ -15,13 +15,49 @@ class MainHomeBinding extends Bindings {
 class MainHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final viewModel = MainHomeViewModel();
+
     return Scaffold(
-      //appBar: AppBar(title: Text('MainHomePage')),
-      body: Center(
-        child: Container(
-          child: Text(Get.find<MainHomeController>().title.value),
-        ),
+      appBar: AppBar(title: Text('MainHomePage')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ImageSliderWidget(viewModel: viewModel),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class MainHomeViewModel {
+  final List<String> images = [
+    'lib/resources/sale1.jpg',
+    'lib/resources/sale2.png',
+    // Add your image URLs here
+  ];
+}
+
+class ImageSliderWidget extends StatelessWidget {
+  final MainHomeViewModel viewModel;
+
+  ImageSliderWidget({required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return PageView.builder(
+      itemCount: viewModel.images.length,
+      itemBuilder: (context, index) {
+        return Image.asset(
+          viewModel.images[index],
+          fit: BoxFit.fitWidth,
+        );
+      },
     );
   }
 }
